8dafcca4ff210d8053da7ae7f7b216c24d349a4e
dingshu <ds303077135@gmail.com>
2014-11-15 18:42:29 +0800

    Update

--- html/js/app/excited-timeline.js
+++ html/js/app/excited-timeline.js
@@ -3,13 +3,16 @@
 var timelineLi = function (data) {
     // TODO
     this.class = "timeline-list-item-li";
-    this.html = "EXCITED! This is a item demo.";
+    this.avatorSrc = "static/avator-demo.jpeg";
+    this.author = "illusate";
+    this.time = "2014-11-11 11:11:11";
+    this.content = "EXCITED! This is a item demo.";
 };
 
 var timelineNewItemLi = function (num) {
     this.class = "timeline-list-item-new-li";
     if (num)
-        this.html = "View " + num + " excitements!";
+        this.html = "View " + num + " new excitements!";
     else
         this.html = "New excitements!";
 };
@@ -18,6 +21,12 @@ var timelineNewItemLi = function (num) {
 
 var getTimeline = function () {
     return [
+        new timelineLi(),
+        new timelineLi(),
+        new timelineLi(),
+        new timelineLi(),
+        new timelineNewItemLi(4),
+        new timelineLi(),
         new timelineLi(),
         new timelineLi()
     ];
