7da08e1e6900c2ff9e10dadebaea0e46e6cdc7fa
dingshu <ds303077135@gmail.com>
2014-11-16 05:42:41 +0800

    Add new item animation

--- html/js/app/excited-ui.js
+++ html/js/app/excited-ui.js
@@ -78,22 +78,22 @@ $(window).load(function () {
     
     $("#add-new-todo-input").on("keyup", function (event) {
         if(event.keyCode == 13 && this.value){
-            //createNewItem(this.value, nickname);
-            var $newItem = $("<li class='todo-list-item todolist-item-li'>" +
+            createNewItem(this.value, nickname);
+            var $newItem = $("<li class='todo-list-item todolist-item-li new-item-animation'>" +
                              "<a href='#' class='todo-list-item-checkbox'></a>" +
                              "<div class='todo-list-item-content'>" +
                              "<p class='todo-list-item-title'>" + this.value + "</p>" +
                              "<span class='todo-list-item-desc'>" + nickname + "</span>" +
-                             "</div></li>").prependTo("#todo-list-ul").css({
-                "margin-top": "-82px",
-                "opacity": 0
-            }).addClass("new-item-animation").css({
-                "margin-top": 0,
-                "opacity": 1,
-                "-webkit-transform": "rotateZ(0)",
-                "transform": "rotateZ(0)"
-            });
+                             "</div></li>").prependTo("#todo-list-ul");
 
+            setTimeout(function () {
+                $newItem.css({
+                    "margin-top": 0,
+                    "opacity": 1
+                });
+            }, 16);
+
+            this.value = "";
         }
     }).on("blur", function (event) {
     });
