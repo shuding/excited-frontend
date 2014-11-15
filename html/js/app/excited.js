cc2dca2bd633131b5771bbfb06e66a9d11889795
dingshu <ds303077135@gmail.com>
2014-11-15 22:54:31 +0800

    Update overlay

--- html/js/app/excited.js
+++ html/js/app/excited.js
@@ -1,7 +1,39 @@
+// TODO : debug
+var domain = "http://172.27.221.110"
+// TODO : end debug
+
 var app = angular.module("excited", []);
 
-var main = function ($scope) {
+var redirectLogin = function () {
+    $("#overlay").css("display", "inherit");
+    $("#warning").css("opacity", "1");
+    var secondLeft = 5;
+    $("#second").html(secondLeft);
+    setInterval(function () {
+        secondLeft--;
+        if (secondLeft <= 0)
+            window.location.href = "login/";
+        else
+            $("#second").html(secondLeft);
+    }, 1000);
+};
 
+var checkLogin = function () {
+    var signed_in = false;
+    $.ajax({
+        url: domain + "/api/check-login/",
+        method: "get"
+    }).done(function (data) {
+        if (typeof data.signed_in !== "undefined")
+            signed_in = data.signed_in;
+        if (!signed_in)
+            redirectLogin();
+    }).error(function () {
+        redirectLogin();
+    });
+};
+
+var main = function ($scope) {
 };
 
 var timelineList = function ($scope) {
@@ -41,3 +73,5 @@ app.controller("main", main)
     .controller("timeline-list", timelineList)
     .controller("todo-list", todoList)
     .controller("follow-list", followList);
+
+checkLogin();
