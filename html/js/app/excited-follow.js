a3875b46c591ed0e8e95fbee3921107378d3f538
dingshu <ds303077135@gmail.com>
2014-11-16 11:23:59 +0800

    Update followers

--- html/js/app/excited-follow.js
+++ html/js/app/excited-follow.js
@@ -1,18 +1,19 @@
 // objects
 
-var followLi = function (data) {
+var followLi = function (data, desc) {
     // TODO
     this.class = "follow-item-li";
-    this.avator = "static/avator-demo.jpeg";
-    this.name = "illusate";
-    this.desc = "illu";
+    this.avator = "static/" + data + ".png";
+    this.name = data;
+    this.desc = desc;
 };
 
 // functions
 
 var getFollow = function () {
     return [
-        new followLi(),
-        new followLi()
+        new followLi("dribbble", "Show and tell for designers."),
+        new followLi("36kr", "关注互联网创业"),
+        new followLi("bilibili", "( ゜- ゜)つロ  乾杯~")
     ];
 };
